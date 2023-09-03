654faa208db81b259cfe03b6bcf90fe793c758ad
Priizy12 <ivailsonqueiroz9@gmail.com>
2023-09-03 13:46:35 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,13 @@ import 'dotenv/config'
 
 const app = express();
 app.use(express.json())
-app.use(cors({
-    origin: process.env.ENABLED_CORS?.split(';') || []
-}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization")
+    app.use(cors());
+    next();
+});
 app.use(router)
 
 app.listen(3333, () => {
