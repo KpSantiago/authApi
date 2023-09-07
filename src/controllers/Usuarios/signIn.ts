42041fc1f94275241f9fa6f3d2c280f95b649ed9
KpSantiago <santiago.kauajose11@gmail.com>
2023-09-07 14:04:34 -0300

    upgrades added

--- src/controllers/Usuarios/signIn.ts
+++ src/controllers/Usuarios/signIn.ts
@@ -1,76 +1,76 @@
 import { PrismaClient } from "@prisma/client";
 import { RequestHandler, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import * as bcrypt from 'bcryptjs';
-import * as jwt from 'jsonwebtoken'
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
 import { IUsuario } from "../../database/models";
-import { validation } from '../../shared/middlewares/Validation';
-import * as yup from 'yup'
-const prisma = new PrismaClient()
+import { validation } from "../../shared/middlewares/Validation";
+import * as yup from "yup";
+const prisma = new PrismaClient();
 
-
-
-interface IBodyProps extends Omit<IUsuario, 'id' | 'name' | 'cpf' | 'roleId'> {}
-
-
-
-
-
-
+interface IBodyProps extends Omit<IUsuario, "id" | "name" | "cpf" | "roleId"> {}
 
 export const signInValidation = validation((getSchema) => ({
-    body: getSchema<IBodyProps>(yup.object().shape({
-      email: yup.string().required().email().min(5),
-      password: yup.string().required().min(6),
-    })),
-  }));
+	body: getSchema<IBodyProps>(
+		yup.object().shape({
+			email: yup.string().required().email().min(5),
+			password: yup.string().required().min(6),
+		})
+	),
+}));
 
+export const signIn = async (
+	req: Request<{}, {}, IBodyProps>,
+	res: Response
+) => {
+	const { email, password } = req.body;
 
+	try {
+		const user: {
+			id: number;
+			email: string;
+			name: string;
+			password: string;
+			roleId: number;
+		} | null = await prisma.user.findFirst({ where: { email } });
 
-export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>{
-    const {email, password} = req.body
-  
-  try {
-    const user: { id: number; email: string; name: string; password: string } | null   = await prisma.user.findFirst({ where: { email } });
+		if (!user)
+			return res.status(StatusCodes.UNAUTHORIZED).json({
+				default: {
+					error: {
+						msg: "Usuario não encontrado",
+					},
+				},
+			});
 
-    if(!user) return res.status(StatusCodes.UNAUTHORIZED).json({
-        default:{
-            error:{
-                msg: "Usuario não encontrado"
-            }
-        }
-    });
+		const verifyPass = await bcrypt.compare(password, user.password);
 
-    const verifyPass = await bcrypt.compare(password, user.password)
+		if (!verifyPass)
+			return res.status(StatusCodes.UNAUTHORIZED).json({
+				default: {
+					error: {
+						msg: "Email ou senha incorretos",
+					},
+				},
+			});
 
-    if(!verifyPass) return res.status(StatusCodes.UNAUTHORIZED).json({
-        default:{
-            error:{
-                msg: "Email ou senha incorretos"
-            }
-        }
-    });
-     
-   const token =  jwt.sign({id: user.id}, 'secret', {
-        expiresIn: "24h"
-    } )
+		const token = jwt.sign({ id: user.id }, "secret", {
+			expiresIn: "24h",
+		});
 
-    return res.status(StatusCodes.OK).json({
-       msg: "Logado com sucesso",
-       acessToken: token
-
-    })
-    
-  } catch (error) {
-  console.log(error)
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-        default:{
-            error:{
-                msg: "Email ou senha incorretos"
-            }
-        }
-    })
-  }
-
-
-}
+		return res.status(StatusCodes.OK).json({
+			msg: "Logado com sucesso",
+			roleId: user.roleId,
+			acessToken: token,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(StatusCodes.UNAUTHORIZED).json({
+			default: {
+				error: {
+					msg: "Email ou senha incorretos",
+				},
+			},
+		});
+	}
+};
