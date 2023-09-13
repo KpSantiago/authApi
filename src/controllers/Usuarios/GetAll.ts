ebd2608ee2de81165c504e23bdeb88fd0c53751a
KpSantiago <santiago.kauajose11@gmail.com>
2023-09-13 14:11:25 -0300

    updates added

--- src/controllers/Usuarios/GetAll.ts
+++ src/controllers/Usuarios/GetAll.ts
@@ -4,28 +4,25 @@ import { StatusCodes } from "http-status-codes";
 
 const prisma = new PrismaClient();
 
-
-
-
-
-
-
-
-export const GetAll: RequestHandler = async (req, res) =>{
-    try {
-       const response = await prisma.user.findMany({
-        include:{
-            Roles: {
-                select:{
-                    Role: true,
-                
-                }
-            }
-        } 
-      });
-        return res.status(StatusCodes.OK).json(response)
-    } catch (error) {
-        console.log(error)
-        return res.status(StatusCodes.NOT_FOUND).json({msg: "Erro na busca de Usuarios"})
-    }
-}
+export const GetAll: RequestHandler = async (req, res) => {
+	try {
+		const response = await prisma.user.findMany({
+			include: {
+				Roles: {
+					select: {
+						Role: true,
+					},
+				},
+			},
+			orderBy: {
+				id: "asc",
+			},
+		});
+		return res.status(StatusCodes.OK).json(response);
+	} catch (error) {
+		console.log(error);
+		return res
+			.status(StatusCodes.NOT_FOUND)
+			.json({ msg: "Erro na busca de Usuarios" });
+	}
+};
